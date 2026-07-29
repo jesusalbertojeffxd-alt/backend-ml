dc412aaa858ef57145d0d709ba4d960f197afa58
jesusalbertojeffxd-alt <jesusalbertojeffxd@gmail.com>
2026-07-29 07:46:49 -0600

    Update DetalleVentaEntity.java

--- alixxpres/src/main/java/com/jahm/alixxpres/modelo/DetalleVentaEntity.java
+++ alixxpres/src/main/java/com/jahm/alixxpres/modelo/DetalleVentaEntity.java
@@ -9,25 +9,29 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "detalles_venta")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DetalleVentaEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private Integer cantidad;
     private Double precioUnitario;
     private Double subtotal;
 
-    // --- Relaciones de llaves Fk ---------
     @JsonBackReference
     @ManyToOne
-    @JoinColumn(name = "venta_id") // llave foranea de venta
+    @JoinColumn(name = "venta_id")
     private VentaEntity venta;
 
     @ManyToOne
-    @JoinColumn(name = "producto_id") // llave foranea de producto
+    @JoinColumn(name = "producto_id")
     private ProductoEntity producto;
 }
