5bab3122dea97fdd31efd385158095678c0b9c59
jesusalbertojeffxd-alt <jesusalbertojeffxd@gmail.com>
2026-07-29 07:50:24 -0600

    Update ClienteEntity.java

--- alixxpres/src/main/java/com/jahm/alixxpres/modelo/ClienteEntity.java
+++ alixxpres/src/main/java/com/jahm/alixxpres/modelo/ClienteEntity.java
@@ -5,9 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "clientes")
